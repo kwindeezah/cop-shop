d6fd381997f8eb65df71e979fd645458c3e49021
kwindeezah <hadizathmotunrayo@gmail.com>
2024-02-17 14:49:53 +0100

    Added admin and user frontend

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.userpage');
+    }
+
+    public function redirect()
+    {
+        $userType = Auth::user()->usertype;
+
+        if($userType == '1')
+        {
+            return view('admin.home');
+        }
+        else
+        {
+            return view('home.userpage');
+        }
+    }
+}
